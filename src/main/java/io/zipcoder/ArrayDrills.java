75d7f992a24a1e3239e62d56b6c27d88f5b8aea6
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2018-02-20 13:45:03 -0500

    first commit

---
+++ src/main/java/io/zipcoder/ArrayDrills.java
@@ -0,0 +1,76 @@
+package io.zipcoder;
+
+public class ArrayDrills {
+
+
+    /**
+     * Given an array of ints, return true if value appears as either the first or last element in the array.
+     * The array will be length 1 or more.
+     * example : firstLast(6, [1,2,6); // Should return true
+     *           firstLast(6, [1,2,3]); // Should return false
+     */
+    public Boolean firstLast(Integer value, Integer[] input){
+        return null;
+    }
+
+    /**
+     * Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+     * example : sameFirstLast([1,2,3]); // Should return false
+     *           sameFirstLast([1,2,1]); // Should return true
+     */
+    public Boolean sameFirstLast(Integer[] input){
+        return null;
+    }
+
+
+    /**
+     * Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element.
+     * Both arrays will be length 1 or more.
+     * example : commonEnd([1, 2, 3], [7, 3]); // Should return true
+     *           commonEnd([1, 2, 3], [7, 3, 2]); // Should return false
+     */
+    public Boolean commonEnd(Integer[] input1, Integer[] input2){
+        return null;
+    }
+
+    /**
+     * Given an array of ints, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+     * example : rotateLeft([1, 2, 3]); // Should return [2,3,1]
+     *           rotateLeft([5, 11, 9]); // Should return [11,9,5]
+     */
+    public Integer[] rotateLeft(Integer[] input){
+        return null;
+    }
+
+
+    /**
+     * Given an array of ints, figure out which is element in the array is largest,
+     * and set all the other elements to be that value. Return the changed array.
+     * example : maxValue([1, 2, 3]); // Should return [3,3,3]
+     *           maxValue([5, 11, 9]); // Should return [11,11,11]
+     */
+    public Integer[] maxValue(Integer[] input){
+        return null;
+    }
+
+
+    /**
+     * Given 2 int arrays, a and b, return a new array length 2 containing their middle elements, if length is odd.
+     * If the array length is even the sum of the middle 2 elements in the array.
+     * example : middleWay([1, 2, 3], [4,5,6,2]); // Should return [2,11]
+     *           middleWay([5, 1, 2, 9], [3, 4, 5, 5]); // Should return [3, 9]
+     */
+    public Integer[] middleWay(Integer[] input1, Integer[] input2){
+        return null;
+    }
+
+
+    /**
+     * Start with 2 int arrays, a and b, each length 2.
+     * Consider the sum of the values in each array.
+     * Return the array which has the largest sum. In event of a tie, return a.
+     */
+    public Integer[] biggerTwo(Integer[] a, Integer[] b){
+        return null;
+    }
+}
